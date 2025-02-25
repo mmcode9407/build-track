4822cf5520873f2d942b50e271a011d5490cbc32
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-02-25 16:29:57 +0100

    feature: extend password schema with regex

--- src/api/auth/auth.validators.ts
+++ src/api/auth/auth.validators.ts
@@ -2,16 +2,23 @@ import { z } from "zod";
 
 const SignUpFormSchema = z
   .object({
-    username: z.string().min(2, "Username is required"),
-    email: z.string().min(1, "Email is required").email("Email is invalid"),
-    password: z.string().min(1, "Password is required").min(8, "Password is too short."),
+    username: z.string().min(3, "Username must contain at least 3 characters."),
+    email: z.string().min(1, "Email is required.").email("Email is invalid."),
+    password: z
+      .string()
+      .min(1, "Password is required.")
+      .min(8, "Password must contain at least 8 characters.")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+        "Password must contain lowercase and uppercase letters, a digit, and a special character",
+      ),
     password_confirm: z.string(),
   })
   .superRefine((data, ctx) => {
     if (data.password !== data.password_confirm) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
-        message: "Passwords does not match",
+        message: "Passwords do not match.",
         path: ["password_confirm"],
       });
     }
@@ -19,7 +26,14 @@ const SignUpFormSchema = z
 
 const SignInFormSchema = z.object({
   email: z.string().min(1, "Email is required").email("Email is invalid"),
-  password: z.string().min(1, "Password is required").min(8, "Password is too short."),
+  password: z
+    .string()
+    .min(1, "Password is required.")
+    .min(8, "Password must contain at least 8 characters.")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+      "Password must contain lowercase and uppercase letters, a digit, and a special character",
+    ),
 });
 
 export { SignInFormSchema, SignUpFormSchema };
