63dc113e0c1d2be5b62915247e2d14bda5290f5e
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-02-27 10:52:35 +0100

    refactor: move query client and router to separate file; create app file as container for providers and routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,14 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 
-import { routeTree } from "./routeTree.gen";
-import { GlobalStyles } from "./styles/globalStyles";
-import { theme } from "./styles/theme";
-
-const router = createRouter({ routeTree, scrollRestoration: true });
-
-const queryClient = new QueryClient();
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
+import App from "@/App";
 
 const rootElement = document.getElementById("root")!;
 
-if (!rootElement.innerHTML) {
-  const root = createRoot(rootElement);
+const root = createRoot(rootElement);
 
-  root.render(
-    <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </QueryClientProvider>
-    </StrictMode>,
-  );
-}
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
