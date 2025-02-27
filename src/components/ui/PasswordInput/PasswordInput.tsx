2f721eed88a6a51c2328d97880c43fd11f753962
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-02-27 14:13:05 +0100

    refactor: migrate PasswordInput to new structure; separate styles; improve visibility logic

---
+++ src/components/ui/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,25 @@
+﻿import { useState } from "react";
+
+import { Input } from "@/components/ui/Input/Input";
+
+import * as S from "./PasswordInput.styled";
+
+type InputProps = React.ComponentProps<"input">;
+
+const PasswordInput = ({ ...props }: InputProps) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const handleChangeVisibility = () => setIsPasswordVisible((prev) => !prev);
+
+  return (
+    <S.Container>
+      <Input type={isPasswordVisible ? "text" : "password"} {...props} />
+
+      <S.Button variant="icon" size="icon" onClick={handleChangeVisibility}>
+        {isPasswordVisible ? <S.EyeIcon /> : <S.EyeOffIcon />}
+      </S.Button>
+    </S.Container>
+  );
+};
+
+export { PasswordInput };
